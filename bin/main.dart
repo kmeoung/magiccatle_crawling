7a9d86518e8808cb2e731bbf7e01b7fce88db4e5
kmeoung <kmeoung@gmail.com>
2021-12-13 00:02:20 +0900

    이미지 저장 및 내용 저장기능

--- bin/main.dart
+++ bin/main.dart
@@ -1,49 +1,75 @@
 import 'package:web_scraper/web_scraper.dart';
 import 'dart:io';
 import 'package:http/http.dart' show get;
+import 'package:csv/csv.dart';
 
 void main() {
-  getProduct(819);
+  // getProduct(productId: 1761);
   // getMainPage();
+  for (String key in _brands.keys) {
+    getBrandPage(brand: key, brandCode: _brands[key]!);
+  }
 }
 
 enum PRODUCT_IMG_TYPE { MAIN, BANNER }
 
+const Map<String, int> _brands = {
+  '매직캐슬': 155,
+  '건드': 93,
+  '마담알렉산더': 122,
+  '맨하탄-토이': 129,
+  '뮤키킴': 130,
+  '보니카': 145,
+  '로렌스': 123,
+  '에릭칼': 125,
+  '월드넘버원-부': 116,
+  '포크마니스': 96,
+  '푸쉰': 117,
+  '키두지': 108,
+  '키즈-프리퍼드': 92
+};
+
 getMainPage() async {
   const mainUrl = 'https://magiccastle.co.kr/';
   final webScraper = WebScraper(mainUrl);
-  if (await webScraper.loadWebPage('')) {
+  if (await webScraper.loadWebPage('product/list.html?cate_no=155&page=1')) {
     print(webScraper.getAllScripts());
   }
 }
 
-/**
- * 이미지 다운로드 코드
- */
-_downloadImg(
-    {required String img_url,
-    required String productName,
-    required PRODUCT_IMG_TYPE productImgType}) async {
-  //comment out the next two lines to prevent the device from getting
-  // the image from the web in order to prove that the picture is
-  // coming from the device instead of the web.
-  var url = Uri.parse(img_url); // <-- 1
-  var response = await get(url); // <--2
-  // var documentDirectory = await getApplicationDocumentsDirectory();
-  var directoryPath = 'img';
-  var firstPath = '$directoryPath/$productName';
-  var filePathAndName =
-      '$directoryPath/$productName/${productName}_${productImgType == PRODUCT_IMG_TYPE.MAIN ? 'main' : 'banner'}.jpg';
-  //comment out the next three lines to prevent the image from being saved
-  //to the device to show that it's coming from the internet
-  await Directory(firstPath).create(recursive: true); // <-- 1
-  File file2 = new File(filePathAndName); // <-- 2
-  file2.writeAsBytesSync(response.bodyBytes); // <-- 3
+getPopularProducts() async {}
+
+getBrandPage({String brand = '', required int brandCode}) async {
+  const mainUrl = 'https://magiccastle.co.kr/';
+  final webScraper = WebScraper(mainUrl);
+  int pageNo = 1;
+  while (await webScraper
+      .loadWebPage('product/list.html?cate_no=$brandCode&page=$pageNo')) {
+    // 메인 정보 가져오기
+    List<Map<String, dynamic>> products = webScraper.getElement(
+      'div.mt-3 > div.xans-product > div.ec-base-product > div.row > div',
+      ['id'],
+    );
+    // 상품이 없을 경우 종료
+    if (products.length == 0) break;
+
+    for (var product in products) {
+      String allId = product['attributes']['id'];
+      String id = allId.split('_')[1];
+      getProduct(brandName: brand, productId: int.parse(id));
+    }
+    pageNo++;
+  }
 }
 
-void getProduct(int productId) async {
+/**
+ * 상품 정보 가져오기
+ */
+void getProduct({String brandName = '', required int productId}) async {
   final webScraper = WebScraper('https://magiccastle.co.kr');
   if (await webScraper.loadWebPage('/product/1/$productId/')) {
+    List<List<dynamic>> _infoData = [];
+
     // 메인 정보 가져오기
     List<Map<String, dynamic>> infoTitle = webScraper.getElement(
       'div.infoArea > div.info_wrap > div > div.d_info > ul > li > span.info_title',
@@ -57,6 +83,7 @@ void getProduct(int productId) async {
     );
 
     List<Map<String, String>> mInfo = [];
+
     String product_name = '';
     for (int i = 0; i < infoCont.length; i++) {
       String mInfoTitle = infoTitle[i]['title'] is String
@@ -75,34 +102,108 @@ void getProduct(int productId) async {
         if (mInfoTitle == '상품명') product_name = mInfoCont;
       }
     }
+
+    _infoData.add(mInfo);
+
     // 메인 이미지
     List<Map<String, dynamic>> mainImg = webScraper
         .getElement('div.prdImgView > p.prdImg > a > img', ['id', 'src']);
 
     print('메인이미지 URL : https:' + mainImg.single['attributes']['src']);
 
-    _downloadImg(
-        img_url: 'https:' + mainImg.single['attributes']['src'],
-        productName: product_name,
-        productImgType: PRODUCT_IMG_TYPE.MAIN);
-
     // 배너 이미지
     List<Map<String, dynamic>> bannerImg = webScraper
         .getElement('div#tab-responsive-1 > div.cont > p > img', ['id', 'src']);
 
-    print('배너이미지 URL : https://magiccastle.co.kr' +
-        bannerImg[0]['attributes']['src']);
+    String bannerImgUrl = 'https://magiccastle.co.kr';
 
-    _downloadImg(
-        img_url:
-            'https://magiccastle.co.kr' + bannerImg[0]['attributes']['src'],
-        productName: product_name,
-        productImgType: PRODUCT_IMG_TYPE.BANNER);
+    if (bannerImg.length < 1) {
+      List<Map<String, dynamic>> anotherBannerImg = webScraper.getElement(
+          'div#tab-responsive-1 > div.cont > div > img', ['id', 'src']);
+      bannerImgUrl = bannerImgUrl + anotherBannerImg[0]['attributes']['src'];
+    } else {
+      bannerImgUrl = bannerImgUrl + bannerImg[0]['attributes']['src'];
+    }
+
+    print(bannerImgUrl);
+
+    List<String> mInfoDetail = [];
 
     List<Map<String, dynamic>> table = webScraper
         .getElement('div#tab-responsive-1 > div.cont > table > tbody > tr', []);
     for (var map in table) {
-      print(map['title']);
+      String title = map['title'];
+      mInfoDetail.add(title);
     }
+    _infoData.add(mInfoDetail);
+
+    _downloadImg(
+        img_url: 'https:' + mainImg.single['attributes']['src'],
+        brandName: brandName,
+        productName: product_name,
+        productImgType: PRODUCT_IMG_TYPE.MAIN);
+
+    _downloadImg(
+        img_url: bannerImgUrl,
+        brandName: brandName,
+        productName: product_name,
+        productImgType: PRODUCT_IMG_TYPE.BANNER);
+
+    writeInfoFile(
+        datas: _infoData, productName: product_name, brandName: brandName);
   }
+}
+
+writeInfoFile(
+    {String brandName = "",
+    required String productName,
+    required List<List<dynamic>> datas}) async {
+  if (productName.contains('/')) productName = productName.replaceAll('/', '_');
+  productName = productName.trim();
+  productName = productName.replaceAll(RegExp(r"\s+"), '_');
+
+  var directoryPath = 'data';
+  var firstPath =
+      '$directoryPath/' + '${brandName.isNotEmpty ? '$brandName' : ''}';
+  var secondPath = firstPath + '/$productName';
+
+  var filePathAndName = '$secondPath/$productName.csv';
+
+  await Directory(firstPath).create(recursive: true); // <-- 1
+  await Directory(secondPath).create(recursive: true); // <--
+
+  String csv = const ListToCsvConverter().convert(datas);
+
+  File file2 = new File(filePathAndName); // <-- 2
+  await file2.writeAsString(csv); // <-- 3
+}
+
+/***
+ ** 이미지 다운로드 코드
+ **/
+_downloadImg(
+    {required String img_url,
+    String brandName = "",
+    required String productName,
+    required PRODUCT_IMG_TYPE productImgType}) async {
+  if (productName.contains('/')) productName = productName.replaceAll('/', '_');
+  productName = productName.trim();
+  productName = productName.replaceAll(RegExp(r"\s+"), '_');
+
+  var url = Uri.parse(img_url); // <-- 1
+  var response = await get(url); // <--2
+
+  var directoryPath = 'data';
+  var firstPath =
+      '$directoryPath/' + '${brandName.isNotEmpty ? '$brandName' : ''}';
+  var secondPath = firstPath + '/$productName';
+
+  var filePathAndName =
+      '$secondPath/${productName}_${productImgType == PRODUCT_IMG_TYPE.MAIN ? 'main' : 'banner'}.jpg';
+
+  await Directory(firstPath).create(recursive: true); // <-- 1
+  await Directory(secondPath).create(recursive: true); // <--
+
+  File file2 = new File(filePathAndName); // <-- 2
+  await file2.writeAsBytes(response.bodyBytes); // <-- 3
 }
